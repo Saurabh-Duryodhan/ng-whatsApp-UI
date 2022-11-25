039b2c25b41e5ddab4cea3278b3d6e6ffe12666f
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2022-11-25 15:55:27 +0530

    ng-whatsApp-UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  message = '';
+
+  handleInput(event: Event) {
+    this.message = (<HTMLInputElement>event.target).value;
+  }
+
+  sendMessage() {
+    const elems = document.createElement('p');
+    elems.innerHTML = this.message;
+    elems.classList.add('message');
+    const userMessage = document
+      .querySelector('.user-message')
+      ?.appendChild(elems);
+    console.log(userMessage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FailedComponent } from './failed/failed.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FailedComponent, SuccessComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
